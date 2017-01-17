eebc29ed80ef707d05b00bf3238ceb5b5fe337e1
jianfei_mars@hotmail.com <jianfei@yangjianfeideMacBook-Pro.local>
2017-01-18 00:25:22 +0800

    fix pre

--- LSTM_sequence_classification/Simpe_LSTM_sc.py
+++ LSTM_sequence_classification/Simpe_LSTM_sc.py
@@ -41,7 +41,6 @@ scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
 
 
-
 # Running result
 # ____________________________________________________________________________________________________
 # Layer (type)                     Output Shape          Param #     Connected to                     
