d69d92931ac076dd43a3211f78807965e4797eb2
marsrocky <jianfei_mars@hotmail.com>
2017-01-26 15:29:59 +0800

    csi data matlab acquiring

---
+++ CSI_data_processing/CSI_data_acquiring/position_record.m
@@ -0,0 +1,41 @@
+clear all;
+close all;
+no_position=6;
+total_port={'8866', '8867'};
+no_package=10;
+packet_actual=1;
+
+for i=1:no_position
+    for j=1:length(total_port)
+        for k=1:no_package
+            fileName = strcat(int2str(i), '\', total_port(j), '-', int2str(k));
+            read_log_file_be (fileName{1})
+            Data = ans;
+            
+            %packet analysis
+            packet1 = Data{1};
+            if isempty(packet1)==0
+                if isempty(packet1.csi)==0
+                    if mean(squeeze(packet1.csi(1,1,:))')~=0
+                        csidata = packet1.csi;
+                        csi_rawdata{i}{j}{k} = squeeze(csidata(:,1,:));
+                    end
+                end
+            end
+        end
+    end
+end
+
+save csi_rawdata csi_rawdata
+% calculate raw mag and phase
+for i=1:no_position
+    for j=1:length(total_port)
+        for k=1:no_package
+            csi_mag{i}{j}{k}=db(csi_rawdata{i}{j}{k});
+            csi_phase{i}{j}{k}=angle(csi_rawdata{i}{j}{k});
+        end
+    end
+end
+
+save csi_mag csi_mag
+save csi_phase csi_phase
