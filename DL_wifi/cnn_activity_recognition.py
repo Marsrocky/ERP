b0d510fcfebe63a4d6c84527bf0c08d1e1421259
jianfei_mars@hotmail.com <jianfei_mars@hotmail.com>
2017-07-26 13:31:19 +0800

    CNN

---
+++ DL_wifi/cnn_activity_recognition.py
@@ -0,0 +1,106 @@
+# coding=utf-8
+# Author=Jianfei
+
+import tensorflow as tf
+import numpy as np
+
+# 超参数
+learning_rate = 0.001
+training_iters = 200000
+batch_size = 64
+display_step = 20
+
+# 网络参数
+n_input = 114*114;
+n_classes = 4;
+dropout = 0.8;
+
+# 占位符
+x = tf.placeholder(tf.float32, [None, n_input])
+y = tf.placeholder(tf.float32, [None, n_classes])
+keep_prob = tf.placeholder(tf.float32)
+
+# 卷积操作
+def conv2d(name, l_input, w, b):
+	return tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(l_input, w, strides=[1, 1, 1, 1], padding='SAME'),b), name=name)
+
+# 最大向下采样
+def max_pool(name, l_input, k):
+	return tf.nn.max_pool(l_input, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME', name=name)
+
+# 归一化操作
+def norm(name, l_input, lsize=4):
+    return tf.nn.lrn(l_input, lsize, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name=name)
+
+
+def wifi_net(_X, _weights, _biases, _dropout):
+	''' Model for WiFi based Activity Recognition'''
+	_X = tf.reshape(_X, shape=[-1, 114, 114, 1])
+
+	# Convolutional Layer #1
+	conv1 = conv2d('conv1', _X, _weights['wc1'], _biases['bc1'])
+	pool1 = max_pool('pool1', conv1, k=2)
+	norm1 = norm('norm1', pool1, lsize=4)
+	norm1 = tf.nn.dropout(norm1, _dropout)
+
+	# Convolutional Layer #2
+	conv2 = conv2d('conv2', norm1, _weights['wc2'], _biases['bc2'])
+	pool2 = max_pool('pool2', conv2, k=2)
+	norm2 = norm('norm2', pool2, lsize=4)
+	norm2 = tf.nn.dropout(norm2, _dropout)
+
+	# Convolutional Layer #3
+	conv3 = conv2d('conv3', norm2, _weights['wc3'], _biases['bc3'])
+	pool3 = max_pool('pool3', conv3, k=2)
+	norm3 = norm('norm3', pool3, lsize=4)
+	norm3 = tf.nn.dropout(norm3, _dropout)
+
+	# Dense Layer
+	# 特征图 ---> 向量
+	dense1 = tf.reshape(norm3, [-1, _weights['wd1'].get_shape().as_list()[0]])
+	dense1 = tf.nn.relu(tf.matmul(dense1, _weights['wd1']) + _biases['bd1'], name='fc1')
+	dense2 = tf.nn.relu(tf.matmul(dense1, _weights['wd2']) + _biases['bd2'], name='fc2')
+
+	# Output Layer
+	out = tf.matmul(dense2, _weights['out']) + _biases['out']
+	return out
+
+# Save all weights
+weights = {
+    'wc1': tf.Variable(tf.random_normal([3, 3, 1, 64])),
+    'wc2': tf.Variable(tf.random_normal([3, 3, 64, 128])),
+    'wc3': tf.Variable(tf.random_normal([3, 3, 128, 256])),
+    'wd1': tf.Variable(tf.random_normal([4*4*256, 1024])),
+    'wd2': tf.Variable(tf.random_normal([1024, 1024])),
+    'out': tf.Variable(tf.random_normal([1024, n_classes]))
+}
+biases = {
+    'bc1': tf.Variable(tf.random_normal([64])),
+    'bc2': tf.Variable(tf.random_normal([128])),
+    'bc3': tf.Variable(tf.random_normal([256])),
+    'bd1': tf.Variable(tf.random_normal([1024])),
+    'bd2': tf.Variable(tf.random_normal([1024])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+# 构建模型
+pred = wifi_net(x, weights, biases, keep_prob)
+
+# 定义损失函数和学习步骤
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# 测试网络
+correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+# 初始化所有的共享变量
+init = tf.global_variables_initializer()
+
+
+
+
+
+
+
+
